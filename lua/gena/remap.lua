0605899bb441c20d6d806bb572069648b7f18479
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2024-10-06 20:47:48 -0300

    small updates

--- lua/gena/remap.lua
+++ lua/gena/remap.lua
@@ -28,11 +28,9 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-vim.keymap.set("n", "<leader>k", "<cmd>!kubectl apply -f %<CR>", { silent = true })
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -46,3 +44,8 @@ vim.keymap.set(
     "<leader>ee",
     "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
 )
+
+vim.keymap.set("n", "<leader>c", function()
+    local char = vim.fn.input("")
+    vim.cmd("g/" .. char .. "\\|^$/d")
+end)
