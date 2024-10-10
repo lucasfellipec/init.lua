9eea359deb9a9630ad35569c9e902b576cf6aeab
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2024-10-10 08:50:52 -0300

    small updates

--- lua/gena/remap.lua
+++ lua/gena/remap.lua
@@ -21,10 +21,10 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
