0b6e445a1882f09d72457556e6b59d7168049277
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2024-12-27 16:12:29 -0300

    refactor(telescope): update keymaps and remove commented code
    - remove unused harpoon-related code
    - change keymaps for find_files and git_files
    - update file ignore patterns

--- lua/gena/lazy/telescope.lua
+++ lua/gena/lazy/telescope.lua
@@ -5,36 +5,13 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        -- local harpoon = require('harpoon')
-        -- harpoon:setup({})
-        --
-        -- local conf = require("telescope.config").values
-        -- local function toggle_telescope(harpoon_files)
-        --     local file_paths = {}
-        --     for _, item in ipairs(harpoon_files.items) do
-        --         table.insert(file_paths, item.value)
-        --     end
-        --
-        --     require("telescope.pickers").new({}, {
-        --         prompt_title = "Harpoon",
-        --         finder = require("telescope.finders").new_table({
-        --             results = file_paths,
-        --         }),
-        --         previewer = conf.file_previewer({}),
-        --         sorter = conf.generic_sorter({}),
-        --     }):find()
-        -- end
-        --
-        -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-        --     { desc = "Open harpoon window" })
-
         require("telescope").setup({
             defaults = { file_ignore_patterns = { "node_modules" } },
         })
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>gt", builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set("n", "<leader>pws", function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
