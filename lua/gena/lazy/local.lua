f30d06b78de01e8b1f8d6ba2109efff7a8bacc8e
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2024-12-30 20:38:48 -0300

    feat(colorscheme): add dynamic colorscheme switching based on filetype
    - add autocmd to switch colorscheme for c files
    - include gruber-darker as a local plugin

--- lua/gena/lazy/local.lua
+++ lua/gena/lazy/local.lua
@@ -1,7 +1,13 @@
 local local_plugins = {
     {
+        "gruber-darker",
+        dir = "~/personal/gruber-darker.nvim",
+        config = function() end
+    },
+    {
         "ThePrimeagen/vim-be-good",
     },
+
 }
 
 return local_plugins
