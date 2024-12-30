a114b4d4f224345e90016b9a488be80c6995a500
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2024-12-30 14:42:57 -0300

    refactor(colors): switch default colorscheme to gruber-darker
    - comment out rose-pine-moon as default
    - uncomment gruber-darker as default
    - remove blazkowolf/gruber-darker.nvim plugin
    - add local gruber-darker plugin to local.lua

--- lua/gena/lazy/colors.lua
+++ lua/gena/lazy/colors.lua
@@ -1,6 +1,6 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine-moon"
-    -- color = color or "gruber-darker"
+    -- color = color or "rose-pine-moon"
+    color = color or "gruber-darker"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -8,19 +8,6 @@ function ColorMyPencils(color)
 end
 
 return {
-    {
-        "blazkowolf/gruber-darker.nvim",
-        opts = {
-            italic = {
-                strings = false,
-                comments = false,
-                operators = false,
-                folds = false,
-            },
-            undercurl = false,
-            underline = false,
-        },
-    },
     {
         "rose-pine/neovim",
         name = "rose-pine",
--- lua/gena/lazy/local.lua
+++ lua/gena/lazy/local.lua
@@ -1,3 +1,13 @@
-return {
-    "ThePrimeagen/vim-be-good",
+local local_plugins = {
+    {
+        "ThePrimeagen/vim-be-good",
+    },
+    {
+        "gruber-darker",
+        dir = "~/personal/gruber-darker.nvim",
+        config = function()
+        end
+    },
 }
+
+return local_plugins
