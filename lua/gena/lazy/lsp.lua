a1a5336cba591dd01df57bb5e7b74be1a5304aff
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2025-01-16 21:25:59 -0300

    small updates

--- lua/gena/lazy/lsp.lua
+++ lua/gena/lazy/lsp.lua
@@ -37,6 +37,7 @@ return {
                 "gopls",
                 "zls",
                 "elixirls",
+                "bashls",
             },
             handlers = {
                 function(server_name)
