f445de2955da359086b93f7581f58fb5ed30614c
Lucas Fellipe Carvalho Moreira <lucas.fellipe.c@gmail.com>
2025-07-01 10:19:44 -0300

    automated dev commit

--- lua/rabbithole/init.lua
+++ lua/rabbithole/init.lua
@@ -51,5 +51,5 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_liststyle = 1
-vim.g.netrw_sort_options = "i"
+-- vim.g.netrw_sort_options = "i"
 vim.g.netrw_sort_by = "exten"
